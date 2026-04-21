56be224276aedf5e6505ab5f60dfee5a3153108d
King_Jace <kingj2@rhodes.edu>
2026-04-21 09:39:44 -0500

    Particool sample

--- particool/sample.rkt
+++ particool/sample.rkt
@@ -1,18 +1,18 @@
 #lang particool
 
 system Earth {
-              particles:1000
+              particles:1
               center:(150,150)
               velocity:(0,0)
               ; total mass?
               ; particle size? / system radius?
               }
 
-;system Tundura {
-;                particles:0
-;                center:(40,20)
-;                velocity:(1,1)
-;               }
+system Moon {
+                particles:1
+                center:(150,100)
+                velocity:(1,0)
+               }
 
 simulation {
             size:(700,700)
